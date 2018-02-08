91fe0c066252e5d1418f2053f247039b09941e94
Ellen Tseng <33066419+elltseng@users.noreply.github.com>
2018-02-07 19:35:46 -0800

    New assets

---
+++ Assets/Scripts/VRButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VRButton : MonoBehaviour {
+
+	public Image Next;
+	public Color NormalColor;
+	public Color HighlightColor;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public void OnGazeEnter() 
+	{
+		Next.color = HighlightColor;
+	}
+
+	public void OnGazeExit()
+	{
+		Next.color = NormalColor;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(OVRInput.Get(OVRInput.Button.One))
+		{
+			Debug.Log("works");
+		}
+		
+	}
+
+	
+
+	
+}
